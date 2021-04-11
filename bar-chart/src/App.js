7538a11266caaee0c6f20db745ddbf4ebd4a1b98
ZoeyF75 <zf.zoodle@gmail.com>
2021-04-11 02:21:20 -0700

    Example chart, going to test out different chart api's/ frameworks firt

--- bar-chart/src/App.js
+++ bar-chart/src/App.js
@@ -1,31 +1,11 @@
 import Form from './components/Form';
 import Graph from './components/Graph';
-import CanvasJSReact from './assets/canvasjs.react';
-var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 function App() {
-  
-    const options = {
-      title: {
-        text: "Basic Column Chart in React"
-      },
-      data: [{				
-                type: "column",
-                dataPoints: [
-                    { label: "Apple",  y: 10  },
-                    { label: "Orange", y: 15  },
-                    { label: "Banana", y: 25  },
-                    { label: "Mango",  y: 30  },
-                    { label: "Grape",  y: 28  }
-                ]
-       }]
-   }
-
   return (
     <div>
       <Form title="Graph Info"/>
       <Graph />
-      <CanvasJSChart options = {options} />
     </div>
   );
 }
--- bar-chart/src/components/Graph.js
+++ bar-chart/src/components/Graph.js
@@ -1,7 +1,25 @@
+import CanvasJSReact from '../assets/canvasjs.react';
+const CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
 const Graph = () => {
+  const options = {
+    title: {
+      text: "Basic Column Chart in React"
+    },
+    data: [{				
+              type: "column",
+              dataPoints: [
+                  { label: "Apple",  y: 10  },
+                  { label: "Orange", y: 15  },
+                  { label: "Banana", y: 25  },
+                  { label: "Mango",  y: 30  },
+                  { label: "Grape",  y: 28  }
+              ]
+     }]
+ }
   return (
     <article class="container">
-      Hello
+      <CanvasJSChart options = {options} />
     </article>
   )
 }
