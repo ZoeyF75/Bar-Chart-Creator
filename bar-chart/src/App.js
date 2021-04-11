3e450d0a900a5821fcf33123a6571268c87c3273
Zoey Farago <zoeyfarago@Zoeys-MacBook-Pro.local>
2021-04-10 22:16:56 -0700

    Nav bar

--- bar-chart/src/App.js
+++ bar-chart/src/App.js
@@ -1,8 +1,11 @@
+import Header from './components/Header';
+
 function App() {
   return (
-    <div className="form">
+    <div className="container">
       <h1>
         Hello From React</h1>
+        <Header title="Graph Info"/>
     </div>
   );
 }
